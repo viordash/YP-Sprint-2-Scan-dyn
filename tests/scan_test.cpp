39b16ac253658d548f56a763c403543e42c48e7a
Vitalii Orazov <viordash@yandex.ru>
2025-07-20 13:23:00 +0000

    tests: simplify

--- tests/scan_test.cpp
+++ tests/scan_test.cpp
@@ -4,39 +4,42 @@
 
 #include "scan.hpp"
 
+using namespace stdx::details;
+
 TEST(ScanTest, test_is_supported_type_v) {
-    ASSERT_TRUE(stdx::details::is_supported_type_v<int8_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<int16_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<int32_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<int64_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<uint8_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<uint16_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<uint32_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<uint64_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<float>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<double>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<std::string_view>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<std::string>);
+    ASSERT_TRUE(is_supported_type_v<int8_t>);
+    ASSERT_TRUE(is_supported_type_v<int16_t>);
+    ASSERT_TRUE(is_supported_type_v<int32_t>);
+    ASSERT_TRUE(is_supported_type_v<int64_t>);
+    ASSERT_TRUE(is_supported_type_v<uint8_t>);
+    ASSERT_TRUE(is_supported_type_v<uint16_t>);
+    ASSERT_TRUE(is_supported_type_v<uint32_t>);
+    ASSERT_TRUE(is_supported_type_v<uint64_t>);
+    ASSERT_TRUE(is_supported_type_v<float>);
+    ASSERT_TRUE(is_supported_type_v<double>);
+    ASSERT_TRUE(is_supported_type_v<std::string_view>);
+    ASSERT_TRUE(is_supported_type_v<std::string>);
 
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const int8_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const int16_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const int32_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const int64_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const uint8_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const uint16_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const uint32_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const uint64_t>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const float>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const double>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const std::string_view>);
-    ASSERT_TRUE(stdx::details::is_supported_type_v<const std::string>);
+    ASSERT_TRUE(is_supported_type_v<const int8_t>);
+    ASSERT_TRUE(is_supported_type_v<const int16_t>);
+    ASSERT_TRUE(is_supported_type_v<const int32_t>);
+    ASSERT_TRUE(is_supported_type_v<const int64_t>);
+    ASSERT_TRUE(is_supported_type_v<const uint8_t>);
+    ASSERT_TRUE(is_supported_type_v<const uint16_t>);
+    ASSERT_TRUE(is_supported_type_v<const uint32_t>);
+    ASSERT_TRUE(is_supported_type_v<const uint64_t>);
+    ASSERT_TRUE(is_supported_type_v<const float>);
+    ASSERT_TRUE(is_supported_type_v<const double>);
+    ASSERT_TRUE(is_supported_type_v<const std::string_view>);
+    ASSERT_TRUE(is_supported_type_v<const std::string>);
 
-    ASSERT_FALSE(stdx::details::is_supported_type_v<char>);
-    ASSERT_FALSE(stdx::details::is_supported_type_v<void *>);
-    
-    ASSERT_FALSE(stdx::details::is_supported_type_v<const char>);
-    ASSERT_FALSE(stdx::details::is_supported_type_v<const void *>);
+    ASSERT_FALSE(is_supported_type_v<char>);
+    ASSERT_FALSE(is_supported_type_v<void *>);
+
+    ASSERT_FALSE(is_supported_type_v<const char>);
+    ASSERT_FALSE(is_supported_type_v<const void *>);
 }
+
 
 TEST(ScanTest, SimpleTest) {
     auto result = stdx::scan<std::string>("number", "{}");
